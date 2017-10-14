5e8fd7a9759abe9a207d0155a78ba1f13b3c24d9
mikesuchor@gmail.com <mikesuchor@gmail.com>
2017-10-14 16:09:25 -0400

    Fixed game logic

--- script.js
+++ script.js
@@ -10,116 +10,154 @@ User Story: I can choose whether I want to play as X or O.
 var board = [["", "", ""], ["", "", ""], ["", "", ""]];
 var player;
 var computer;
+var squaresLeft = 9;
 var cpuRow;
 var cpuColumn;
 
-// Ways to win for player
-function testPlayerWin() {
-  if (
-    (board[0][0] === player && board[0][1] === player && board[0][2] === player) ||
-    (board[1][0] === player && board[1][1] === player && board[1][2] === player) ||
-    (board[2][0] === player && board[2][1] === player && board[2][2] === player) ||
-    (board[0][0] === player && board[1][0] === player && board[2][0] === player) ||
-    (board[0][1] === player && board[1][1] === player && board[2][1] === player) ||
-    (board[0][2] === player && board[1][2] === player && board[2][2] === player) ||
-    (board[0][0] === player && board[1][1] === player && board[2][2] === player) ||
-    (board[0][2] === player && board[1][2] === player && board[2][2] === player)
-  ) {
-    alert("Player (" + player + ") wins!");
-    disableBoard();
-    return(true);
-  }
-}
-
-function testComputerWin() {
-  if (
-    (board[0][0] === computer && board[0][1] === computer && board[0][2] === computer) ||
-    (board[1][0] === computer && board[1][1] === computer && board[1][2] === computer) ||
-    (board[2][0] === computer && board[2][1] === computer && board[2][2] === computer) ||
-    (board[0][0] === computer && board[1][0] === computer && board[2][0] === computer) ||
-    (board[0][1] === computer && board[1][1] === computer && board[2][1] === computer) ||
-    (board[0][2] === computer && board[1][2] === computer && board[2][2] === computer) ||
-    (board[0][0] === computer && board[1][1] === computer && board[2][2] === computer) ||
-    (board[0][2] === computer && board[1][2] === computer && board[2][2] === computer)
-  ) {
-    alert("Computer (" + computer + ") wins!");
-    disableBoard();
-    return(true);
-  }
-}
-
-/* Assigns the player and computer as X or O based on the button pressed at the start of the game */
+/* Assigns the player and computer as X or O based on the button pressed at the start of the game, if O is chosen the computer moves first */
 function chooseXorO(symbol) {
   if (symbol === "X") {
     player = "X";
     computer = "O";
-    disableXO();
+    disableXO(symbol);
     enableBoard();
   } else {
     player = "O";
     computer = "X";
-    disableXO();
+    disableXO(symbol);
     enableBoard();
+    computerMove();
   }
 }
 
 /* Disables the X and O buttons after they are clicked to start the game */
-function disableXO() {
-  document.getElementById("xbutton").disabled = true;
-  document.getElementById("obutton").disabled = true;
+function disableXO(symbol) {
+  if (symbol === "X") {
+    document.getElementById("xbutton").disabled = true;
+    document.getElementById("xbutton").style.color = "orange";
+    document.getElementById("obutton").disabled = true;
+    document.getElementById("obutton").style.color = "black";
+    document.getElementById("obutton").style.border = "none";
+  } else {
+    document.getElementById("xbutton").disabled = true;
+    document.getElementById("xbutton").style.color = "black";
+    document.getElementById("xbutton").style.border = "none";
+    document.getElementById("obutton").disabled = true;
+    document.getElementById("obutton").style.color = "orange";
+  }
 }
 
 /* Enables clicking of buttons on the board */
 function enableBoard() {
-  var x = document.getElementsByClassName("box");
-  for (var i = 0; i < x.length; i++) {
-    x[i].disabled = false;
+  var boxes = document.getElementsByClassName("box");
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].innerHTML = "";
+    boxes[i].setAttribute("onclick", "userMove(id)");
   }
+}
+
+/* When the user clicks a square, disables the square, and tests if the player has won, if it's a tie game, then generates a computer move and checks if the computer has won */
+function userMove(id) {
+  document.getElementById(id).removeAttribute("onclick");
+  document.getElementById(id).innerText = player;
+  id = id.split("");
+  board[id[0]][id[1]] = player;
+  squaresLeft--;
+  if (testWin(player)) {
+    return;
+  }
+  if (squaresLeft === 0) {
+    tieGame();
+    return;
+  }
+  setTimeout(function() {
+    computerMove();
+  }, 250);
+  if (testWin(computer)) {
+    return;
+  }
+}
+
+/* Tests if 3 symbols in a row are the same and ends the game if true */
+function testWin(symbol) {
+  if (
+    (board[0][0] === symbol &&
+      board[0][1] === symbol &&
+      board[0][2] === symbol) ||
+    (board[1][0] === symbol &&
+      board[1][1] === symbol &&
+      board[1][2] === symbol) ||
+    (board[2][0] === symbol &&
+      board[2][1] === symbol &&
+      board[2][2] === symbol) ||
+    (board[0][0] === symbol &&
+      board[1][0] === symbol &&
+      board[2][0] === symbol) ||
+    (board[0][1] === symbol &&
+      board[1][1] === symbol &&
+      board[2][1] === symbol) ||
+    (board[0][2] === symbol &&
+      board[1][2] === symbol &&
+      board[2][2] === symbol) ||
+    (board[0][0] === symbol &&
+      board[1][1] === symbol &&
+      board[2][2] === symbol) ||
+    (board[0][2] === symbol && board[1][1] === symbol && board[2][0] === symbol)
+  ) {
+    alert(symbol + " wins!");
+    disableBoard();
+    return true;
+  }
+}
+
+/* Generates a random computer move in an empty square and disables that square */
+function computerMove() {
+  do {
+    generateRandomMove();
+  } while (
+    board[cpuRow][cpuColumn] === player ||
+    board[cpuRow][cpuColumn] === computer
+  );
+  board[cpuRow][cpuColumn] = computer;
+  document.getElementById(cpuRow + "" + cpuColumn).innerHTML = computer;
+  document.getElementById(cpuRow + "" + cpuColumn).removeAttribute("onclick");
+  squaresLeft--;
 }
 
 /* Disables clicking of buttons on the board */
 function disableBoard() {
-  var x = document.getElementsByClassName("box");
-  for (var i = 0; i < x.length; i++) {
-    x[i].disabled = true;
+  var boxes = document.getElementsByClassName("box");
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].removeAttribute("onclick");
   }
 }
 
-function playerMove(id, value) {
-  document.getElementById(id).disabled = true;
-  document.getElementById(id).innerText = player;
-  id = id.split("");
-  board[id[0]][id[1]] = player;
-  if(testPlayerWin()) {
-    return;
-  }
-  computerMove();
-  if(testComputerWin()) {
-    return;
-  }
-}
-
-function computerMove() {
-  do {
-    generateRandomMove();
-  } while ((board[cpuRow][cpuColumn] === player) || (board[cpuRow][cpuColumn] === computer));
-  board[cpuRow][cpuColumn] = computer;
-  document.getElementById(cpuRow + '' + cpuColumn).disabled = true;
-  document.getElementById(cpuRow + '' + cpuColumn).innerHTML = computer;
-}
-
+/* Random number generation for use in computer square selection*/
 function generateRandomMove() {
   cpuRow = Math.floor(Math.random() * 3);
   cpuColumn = Math.floor(Math.random() * 3);
 }
 
+/* Ends the game in a tie */
+function tieGame() {
+  alert("The game is a tie!");
+  disableBoard();
+}
+
+/* Resets the game */
 function gameReset() {
   board = [["", "", ""], ["", "", ""], ["", "", ""]];
+  squaresLeft = 9;
   document.getElementById("xbutton").disabled = false;
+  document.getElementById("xbutton").style.color = "white";
+  document.getElementById("xbutton").style.border = "1px solid white";
   document.getElementById("obutton").disabled = false;
+  document.getElementById("obutton").style.color = "white";
+  document.getElementById("obutton").style.border = "1px solid white";
   var boxes = document.getElementsByClassName("box");
   for (var i = 0; i < boxes.length; i++) {
     boxes[i].innerHTML = "";
+    boxes[i].setAttribute("onclick", "userMove(id)");
     disableBoard();
   }
 }
